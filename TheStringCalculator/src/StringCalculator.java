be1cb9eac3bbaca7bfee156f2cdb63af0e7416d2
divy jain <jainboby17@gmail.com>
2021-08-27 02:49:33 +0530

    Wrote the code to pass the failing test for adding two numbers by splitting the string with commas and add the value after converting them into integers

--- TheStringCalculator/src/StringCalculator.java
+++ TheStringCalculator/src/StringCalculator.java
@@ -6,6 +6,12 @@ public class StringCalculator {
 		if(input.equals("")){
 			return 0;
 		}
+		else if(input.contains(",")) {
+			
+			    String[] numberValues = input.split(",");
+			    return Integer.parseInt(numberValues[0]) + Integer.parseInt(numberValues[1]);
+			  
+		}
 		else {
 			return Integer.parseInt(input);
 		}
