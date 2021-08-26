4e5a840215f91be47c9ac56064fc92e996dfd854
divy jain <jainboby17@gmail.com>
2021-08-27 02:35:03 +0530

    wrote the code by writing if else condition to pass the failing test case

--- TheStringCalculator/src/StringCalculator.java
+++ TheStringCalculator/src/StringCalculator.java
@@ -2,8 +2,15 @@
 
 
 public class StringCalculator {
-	public static int add(String text){
-		return 0;
+	public static int add(String input){
+		if(input.equals("")){
+			return 0;
+		}
+		else {
+			return Integer.parseInt(input);
+		}
+		
+		
 	}
 	
 	
