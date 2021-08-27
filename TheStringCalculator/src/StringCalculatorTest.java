acea8729c1e3eb759267c76067b754544685c60c
divy jain <jainboby17@gmail.com>
2021-08-27 14:51:55 +0530

    Wrote 5th failing Test Case to add multiple numbers with a newLine delimiter

--- TheStringCalculator/src/StringCalculatorTest.java
+++ TheStringCalculator/src/StringCalculatorTest.java
@@ -6,25 +6,28 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void WhenStringIsEmpty() {
+	public void testWhenStringIsEmpty() {
 		  assertEquals(0, StringCalculator.add(""));
 		}
 
 	@Test
-	public void WhenOneNumberAsInput() {
+	public void testWhenOneNumberAsInput() {
 		assertEquals(4,StringCalculator.add("4"));
 	}
 	
 	@Test
-	public void WhenTwoNumbersAsInput() {
+	public void testWhenTwoNumbersAsInput() {
 		assertEquals(9,StringCalculator.add("4,5"));
 	}
 	
 	@Test
-	public void WhenMultipleNumbersAsInput() {
+	public void testWhenMultipleNumbersAsInput() {
 		assertEquals(10,StringCalculator.add("1,4,5"));
 	}
 
-
+	@Test
+	public void testMultipleNumbersWithNewLineDelimiter() {
+		assertEquals(7,StringCalculator.add("1\n2,4"));
+	}
 
 }
