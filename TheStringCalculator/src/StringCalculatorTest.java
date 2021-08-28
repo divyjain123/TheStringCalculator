e8569a5b40bb6f09dac72f12e495f2ddec1b624b
divy jain <jainboby17@gmail.com>
2021-08-28 19:16:36 +0530

    Wrote the failing test case for multiple specified delimiters

--- TheStringCalculator/src/StringCalculatorTest.java
+++ TheStringCalculator/src/StringCalculatorTest.java
@@ -66,5 +66,8 @@ public class StringCalculatorTest {
 		assertEquals(6,StringCalculator.add("//[***]\n1***2***3"));
 	}
 	
+	@Test
+	public void MultipleSpecifiedDelimiters() {
+		assertEquals(6,StringCalculator.add("//[*][%]\n1*2%3"));
 
 }
