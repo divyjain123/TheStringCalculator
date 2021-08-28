5e64669ae42c02e14454cfe02a5d973f7751785d
divy jain <jainboby17@gmail.com>
2021-08-28 19:27:20 +0530

    Wrote the last test case for multiple delimiters of variable length which got passed with the prevous code

--- TheStringCalculator/src/StringCalculatorTest.java
+++ TheStringCalculator/src/StringCalculatorTest.java
@@ -70,4 +70,9 @@ public class StringCalculatorTest {
 	public void MultipleSpecifiedDelimiters() {
 		assertEquals(6,StringCalculator.add("//[*][%]\n1*2%3"));
 	}
+	
+	@Test
+	public void MultiplespecifiedDelimitersWithVariableLength() {
+		assertEquals(6,StringCalculator.add("//[***][%%%]\n1***2%%%3"));
+	}
 }
